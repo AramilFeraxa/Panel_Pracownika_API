f51a526c578040c10151ff067f88cdc1db54d1be
AramilFeraxa <lordvader1505@gmail.com>
2025-05-28 20:36:28 +0200

    fixes, update

--- PanelPracownika/Models/Task.cs
+++ PanelPracownika/Models/Task.cs
@@ -8,5 +8,7 @@ namespace PanelPracownika.Models
         public string Text { get; set; }
         public string DueDate { get; set; }
         public bool Completed { get; set; }
+        public int UserId { get; set; }
     }
+
 }
