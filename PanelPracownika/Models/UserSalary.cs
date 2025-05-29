1d47ec4a45dfe47d558e40a515bccd756d51aaed
AramilFeraxa <aferaxa@gmail.com>
2025-05-29 10:41:42 +0200

    +Salary

---
+++ PanelPracownika/Models/UserSalary.cs
@@ -0,0 +1,14 @@
+﻿namespace PanelPracownika.Models
+{
+    public class UserSalary
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string ContractType { get; set; }
+        public double? HourlyRate { get; set; }
+        public double? MonthlySalary { get; set; }
+
+        public Login User { get; set; }
+    }
+
+}
