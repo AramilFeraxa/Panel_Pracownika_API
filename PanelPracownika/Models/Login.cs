6e11bcdfb28b17c17d7b27b6fa720435611a170a
AramilFeraxa <lordvader1505@gmail.com>
2026-01-08 23:12:23 +0100

    +photo

--- PanelPracownika/Models/Login.cs
+++ PanelPracownika/Models/Login.cs
@@ -19,6 +19,8 @@ namespace PanelPracownika.Models
         public string Surname { get; set; }
         public string Password { get; set; }
         public bool IsAdmin { get; set; }
+        public byte[]? ProfileImage { get; set; }
+        public string? ProfileImageContentType { get; set; }
 
         public string HashPassword(string password)
         {
