68427f3b22a2ca31cb136ee84df50e4d1ca58a76
AramilFeraxa <lordvader1505@gmail.com>
2026-05-05 13:28:38 +0200

    mail update

--- PanelPracownika/Models/EmailSettings.cs
+++ PanelPracownika/Models/EmailSettings.cs
@@ -6,6 +6,8 @@
         public int Port { get; set; }
         public string SenderName { get; set; } = string.Empty;
         public string SenderEmail { get; set; } = string.Empty;
+        public string FromEmail { get; set; } = string.Empty;
+        public string RecipientEmail { get; set; } = string.Empty;
         public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
     }
